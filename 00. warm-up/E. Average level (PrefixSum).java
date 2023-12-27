b87f79c7226a40a4d6e5319d31a3065143691750
KirShelt <114348113+KirShelt@users.noreply.github.com>
2023-12-27 15:15:16 +0300

    Task-004

---
+++ 00. warm-up/E. Average level (PrefixSum).java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        StringTokenizer tk = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(tk.nextToken());
+
+        tk = new StringTokenizer(br.readLine());
+        long[] a = new long[n];
+        long[] prefixSum = new long[n + 1];
+        prefixSum[0] = 0;
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(tk.nextToken());
+            prefixSum[i + 1] = prefixSum[i] + a[i];
+        }
+        StringBuilder resultString = new StringBuilder();
+                for (int i = 0; i < n; i++) {
+            long iResult = a[i]*(2*i+1-n)-prefixSum[i]+prefixSum[n]-prefixSum[i+1];
+            resultString.append(iResult).append(" ");
+        }
+        System.out.println(resultString.toString().trim());
+    }
+}
